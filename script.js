c072549f153f891fd63855910d24fc6741fe9b31
katya20 <kpanchenko21@gmail.com>
2016-10-10 09:28:10 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,18 +4,3 @@
 //}
 //
 //$("#button").click(buttonIsPressed)
-
-function whenClicked() {
-  console.log("function run")
-  return "1111"
-}
-
-var a = whenClicked()
-var b = whenClicked
-
-console.log(a.toString())
-console.log(b.toString())
-
-
-
-
