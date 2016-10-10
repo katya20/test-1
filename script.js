c1e437bdf2a13becc68f9a6ea70454d40bfe3482
katya20 <kpanchenko21@gmail.com>
2016-10-10 09:43:45 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var enter = $("input");
+$("input").keydown(onKeydown)
 
+function onKeydown(e) {
 enter.keydown(function(e) {
   e.keyCode == 13
-  console.log(enter)
+  console.log(enter.)
   console.log("yay")
 })
 
